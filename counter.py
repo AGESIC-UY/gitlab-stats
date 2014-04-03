3a6b036ecc45d9c3d9620aa80aa000b22a6e4b2b
Aníbal Pacheco <anibal.pacheco@agesic.gub.uy>
2014-04-03 18:46:16 -0300

    Parsing also the date of the log line to print its maximum value by project

--- counter.py
+++ counter.py
@@ -4,16 +4,17 @@ from os import listdir
 from collections import Counter
 from itertools import chain
 from fnmatch import fnmatch
+from datetime import datetime
 
 # pattern definition
 patterns = [re.compile(pattern) for pattern in (
     # siempre buscamos requests de tipo GET
     # puede venir un pedido a los archive
-    r'Started GET "/([\w-]+/[\w-]+)/repository/archive\.',
+    r'Started GET "/([\w-]+/[\w-]+)/repository/archive\..*at (\d{4}-\d{2}-\d{2})',
     # o un clone por HTTP
-    r'Started GET "/([\w-]+/[\w-]+)(?:\.git)?/info/refs\?service=git-upload-pack"',
+    r'Started GET "/([\w-]+/[\w-]+)(?:\.git)?/info/refs\?service=git-upload-pack".*at (\d{4}-\d{2}-\d{2})',
     # o un clone por SSH
-    r'Started GET "//api/v3/internal/allowed\?key_id=\d+&action=git-upload-pack&ref=_any&project=([\w-]+/[\w-]+)"'
+    r'Started GET "//api/v3/internal/allowed\?key_id=\d+&action=git-upload-pack&ref=_any&project=([\w-]+/[\w-]+)".*at (\d{4}-\d{2}-\d{2})'
 )]
 
 results = []
@@ -30,6 +31,7 @@ for f in ('production.log', 'production.log.1'):
     results += chain.from_iterable(re.findall(pattern, contents) for pattern in patterns)
 
 # print results
-print('Namespace/Project\tDownloads')
-for project, count in Counter(results).most_common():
-    print('%s\t%d' % (project, count))
+print('Namespace/Project\tDownloads\tLast activity')
+for project, count in Counter(x[0] for x in results).most_common():
+    print('%s\t%d\t%s' % (project, count, max(datetime.strptime(x[1],
+        '%Y-%m-%d').date() for x in results if x[0]==project)))
